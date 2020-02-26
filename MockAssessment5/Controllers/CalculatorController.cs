01d8777059e12654bee01fbe6de97ba5a20e5b34
kghafari <kghafari@users.noreply.github.com>
2020-02-25 23:56:34 -0500

    lab focuses on creating views, forms, and actions. Focus on state management

---
+++ MockAssessment5/Controllers/CalculatorController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MockAssessment5.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        public IActionResult Result(int num1, int num2, string operation)
+        {
+            if(operation == "Plus")
+            {
+                ViewBag.Result = num1 + num2;
+                ViewBag.Operation = "+";
+            }
+            else if (operation == "Minus")
+            {
+                ViewBag.Result = num1 - num2;
+                ViewBag.Operation = "-";
+            }
+            else if (operation == "Multiply")
+            {
+                ViewBag.Result = num1 * num2;
+                ViewBag.Operation = "*";
+            }
+            else if (operation == "Divide")
+            {
+                ViewBag.Result = num1 / num2;
+                ViewBag.Operation = "/";
+            }
+            else //how did you get here hackerman?
+            {
+                ViewBag.Result = 0;
+            }
+
+            return View();
+        }
+    }
+}
